db74d9e043daedd90d5eaddc6f7ad0f59bc9ed33
Daniel Sinai <daniel.sinai.main@gmail.com>
2020-08-14 18:44:00 +0300

    changed to map instead of forEach

--- src/loggerifiers/loggerify-object.js
+++ src/loggerifiers/loggerify-object.js
@@ -2,7 +2,7 @@ const loggerifyObject = (app, handleWrapping, logWrappers, logFeatures, config) 
     const newApp = Object.assign({}, app);
     const appEntries = Object.entries(app);
 
-    appEntries.forEach(([key, value]) => {
+    appEntries.map(([key, value]) => {
         if (typeof value === 'function') {
             newApp[key] = logFeatures.reduce(
                 (decoratedFn, logFeature) => handleWrapping(logWrappers[logFeature], decoratedFn, config[logFeature], key), value);
