3020adc5c9e2998845e5b70cd3bf7e503eb6c016
Daniel Sinai <daniel.sinai.main@gmail.com>
2020-08-14 18:42:36 +0300

    moved annonymous to a const instead of a magic string

--- src/loggerifiers/loggerify-function.js
+++ src/loggerifiers/loggerify-function.js
@@ -1,7 +1,9 @@
+const DEFAULT_NAME = "anonymous";
+
 const loggerifyFunction = (app, handleWrapping, logWrappers, logFeatures, config) => {
 
     return logFeatures.reduce(
-        (decoratedFn, logFeature) => handleWrapping(logWrappers[logFeature], decoratedFn, config[logFeature], "anonymus"), app);
+        (decoratedFn, logFeature) => handleWrapping(logWrappers[logFeature], decoratedFn, config[logFeature], DEFAULT_NAME), app);
 }
 
 module.exports = loggerifyFunction;
